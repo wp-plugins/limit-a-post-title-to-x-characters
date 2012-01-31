5c918155deb270827dd0654901937bb05f0774d0
jpmurray <jpmurray@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-31 19:38:51 +0000

    The plugin is evolving to version 1.1, after some bug squashing.
    git-svn-id: https://plugins.svn.wordpress.org/limit-a-post-title-to-x-characters/tags/1.1@498137 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jpmlc-script.js
+++ js/jpmlc-script.js
@@ -1,3 +1,5 @@
+var unCompteur;
+
 jQuery(document).ready(function(){
 	var maximum = jQuery('#jpmlc_maximum').val();
     jQuery('#title').keyup(function(){
@@ -5,6 +7,7 @@ jQuery(document).ready(function(){
 	});
 	function verification_maximum(elemId){
 		var conteur = jQuery(elemId).val().length;
+		unCompteur = jQuery(elemId).val().length;
 		if(conteur > maximum){
 			jQuery('#jpmlc-conteur').addClass('jpmlc-depasse');
 		}else{
@@ -14,11 +17,13 @@ jQuery(document).ready(function(){
 	}
 	jQuery('#vider-titre').click(function(){jQuery('#title').val("").focus();jQuery('#jpmlc-conteur').html(0);});
     jQuery('#publish').mousedown(function() {
-		if(jQuery('#jpmlc-conteur').html() > maximum){
-			alert('You are over the maximum character count allowed!');
+		if(unCompteur < maximum){
+			//visa, ça va !
 			return false;
 		}else{
-			//visa, ça va !
+			alert('Vous dépassez la limite permise de caractères.'+unCompteur);
+			return false;
+
 		}
 });
 });
